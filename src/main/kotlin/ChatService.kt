ad644bd8f8bf1cb5bae227dfb27fa36a6e325c6a
bezglaz-kb <bezglaz@mail.ru>
2026-06-30 13:03:00 +0300

    Sequences added

--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -21,51 +21,56 @@ class ChatService {
     private var nextMessageId = 1
     private var nextChatId = 1
 
-    private fun getActiveMessagesForChat(chatId:Int): List<Message> =
-       messages.filter {it.chatId == chatId && !it.isDeleted}
+    private fun getActiveMessagesForChat(chatId:Int): Sequence<Message> =
+       messages.asSequence()
+           .filter {it.chatId == chatId && !it.isDeleted}
 
     //1. Сколько чатов не прочитано
     fun getUnreadChatsCount(): Int =
-        chats.filter {!it.isDeleted}.count{chat -> getActiveMessagesForChat(chat.id).any{!it.isRead}
+        chats.asSequence()
+            .filter {!it.isDeleted}
+            .count{chat -> getActiveMessagesForChat(chat.id).any{!it.isRead}
     }
 
     //2. Получить список чатов
-    fun getChats(): List<Chat> = chats.filter {!it.isDeleted}
+    fun getChats(): List<Chat> = chats.asSequence()
+        .filter {!it.isDeleted}
+        .toList()
 
     //3. Получить список последних сообщений из чатов
-    fun getLastMessagesFromChats(): List<String> {
-        val result = mutableListOf<String>()
-        chats.filter {!it.isDeleted}.forEach { chat ->
-            val activeMsgs = getActiveMessagesForChat(chat.id)
-
-            if (activeMsgs.isEmpty()) {
-                result.add("Chat with id ${chat.userId}: no messages")
+    fun getLastMessagesFromChats(): List<String> =
+        chats.asSequence()
+            .filter {!it.isDeleted}
+            .map { chat ->
+            val activeMsgs = getActiveMessagesForChat(chat.id).lastOrNull()
+            if (activeMsgs == null) {
+                "Chat with id ${chat.userId}: no messages"
             } else {
-                result.add("Chat with id ${chat.userId}: ${activeMsgs.last().text}")
+               "Chat with id ${chat.userId}: ${activeMsgs.text}"
             }
         }
-        return result
-    }
+        .toList()
 
    //4. Получить список сообщений из чата по ID собеседника, количество сообщений
    // (все отданные сообщения автоматически считаются прочитанными)
    fun getMessagesFromChat (userId: Int, count: Int): List<Message> {
-       val chat = chats.find{it.userId == userId && !it.isDeleted}
+       val chat = chats.asSequence()
+           .find{it.userId == userId && !it.isDeleted}
            ?: throw TargetIsDeletedException("Chat not found")
-       val activeMsgs = getActiveMessagesForChat(chat.id)
-       val neededMsgs = activeMsgs.takeLast(count)
-       neededMsgs.forEach { it.isRead=true }
-       return neededMsgs
+
+       return getActiveMessagesForChat(chat.id)
+           .toList()
+           .takeLast(count)
+           .onEach { it.isRead = true }
    }
 
    //5. Создать новое сообщение + Создать чат, когда пользователю отправляется первое сообщение
    fun createMessage(userId: Int, text:String): Message {
-       var chat = chats.find {it.userId == userId}
+       val chat = chats.asSequence()
+           .find {it.userId == userId}
+           ?: Chat(id = nextChatId++, userId = userId).also { chats.add(it) }
 
-       if (chat == null) {
-           chat = Chat(id = nextChatId++, userId = userId)
-           chats.add(chat)
-       } else if (chat.isDeleted) {
+       if (chat.isDeleted) {
            chat.isDeleted = false
        }
        val message = Message(
@@ -79,7 +84,8 @@ class ChatService {
 
     //6. Отредактировать сообщение
     fun editMessage (messageId:Int, newText: String): Message {
-        val msg = messages.find {it.id == messageId && !it.isDeleted}
+        val msg = messages.asSequence()
+            .find {it.id == messageId && !it.isDeleted}
             ?: throw TargetIsDeletedException("Message hasn't been found")
         msg.text = newText
         return msg
@@ -87,7 +93,8 @@ class ChatService {
 
     //7. Удалить сообщение
     fun deleteMessage (messageId: Int): Boolean {
-        val msg = messages.find {it.id == messageId && !it.isDeleted}
+        val msg = messages.asSequence()
+            .find {it.id == messageId && !it.isDeleted}
             ?: throw TargetIsDeletedException("Message has been deleted yet")
         msg.isDeleted = true
         return true
@@ -95,10 +102,14 @@ class ChatService {
 
     //8. Удалить чат
     fun deleteChat (userId: Int): Boolean {
-        val chat = chats.find{it.userId == userId && !it.isDeleted}
+        val chat = chats.asSequence()
+            .find{it.userId == userId && !it.isDeleted}
             ?: throw TargetIsDeletedException("Chat hasn't been found")
         chat.isDeleted = true
-        messages.filter{it.chatId == chat.id}.forEach { it.isDeleted = true }
+
+        messages.asSequence()
+            .filter{it.chatId == chat.id}
+            .forEach {it.isDeleted = true}
         return true
     }
 }
